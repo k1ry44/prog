6f41d5cdcfc45bbf4335dc91b8cf51b6946eb5b2
k1ry44 <71671006+k1ry44@users.noreply.github.com>
2020-10-03 01:39:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] partys = new string[n];
+            int j = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string party = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1];
+                j = 0;
+                while(party != partys[j] && partys[j] !=null)
+                {
+                    j++;
+                }
+                partys[j] = party;
+
+            }
+            Console.WriteLine("\n--------------\n");
+            j = 0;
+            while(partys[j] != null)
+            {
+                Console.WriteLine("Party " + partys[j]);
+                j++;
+            }
+            
+           
+            Console.ReadKey();
+        }
+    }
+}
+
+
